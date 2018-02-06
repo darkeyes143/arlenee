fc686988a82d7eb8d8cefb3c9df8fc1d9be4a071
darkeyes143 <36121515+darkeyes143@users.noreply.github.com>
2018-02-06 12:21:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>ASSIGNMENT</title>
+</head>
+
+<h3><b>PERSONAL INFORMATION:</b></h3>
+
+<h3><b>NAME:</b> <u>Alfeche, Arlene T.</u></h3>
+
+<h3><b>ADDRESS:</b> <u>Cayam Colon, City of Naga, Cebu</u></h3>
+
+<h3><b>EMAIL:</b> <u>ar_lyn_30@yahoo.com</u></h3>
+
+<h3><b>AGE:</b> <u>20 yrs. old</u></h3>
+
+<h3><b>DATE OF BIRTH:</b> <u>August 30,1997</u></h3>
+
+<h3><b>CONTACT NUMBER:</b> <u>09771438063</u></h3>
+
+</body>
+
+<hr><b><h1> EDUCATIONAL ATTAINMENT </h1></b></hr>
+
+<table border="1">
+	<tr>
+		<th>Level:</th>
+		<th>School:</th>
+		<th>Address:</th>
+		<th>Academic Year:</th>
+	</tr>
+	<tr>
+		<td>Elementary</td>
+		<td> Colon Elementary School</td>
+		<td>Colon,City of Naga</td>
+		<td>2009-2010</td>
+	</tr>
+	<tr>
+		<td>High School</td>
+		<td> Naga National High School</td>
+		<td>West Poblacion,City of Naga</td>
+		<td>2013-2014</td>
+	</tr>
+	<tr>
+		<td>College</td>
+		<td> Cebu Technological University-Naga Extension Campus</td>
+		<td>Central Poblacion,City of Naga</td>
+		<td>2017-present</td>
+	</tr>
+	
+</table>
+
+<br>
+<hr><b><h1> BIOGRAPHY </h1></b></hr>
+<p>
+	I am Alfeche,Arlene T, a first year-student of CTU-Naga Extension Campus, Central Poblacion, City of Naga,Cebu.
+I am living in Cayam,Colon,City of Naga,Cebu. I have 3 brothers. My two older brothers are currently working and my younger
+brother is still in the elementary.<br>	
+	During my freetime, I usually draw and read stories. I feel like I'm travelling when I am reading and I put myself 
+in the shoes of the character thats why I also feel what they feeling.<br>
+	My dream is to travel places in the world to know their traditionos and life. My goal in life is to be a successful  
+in my chosen career and be able to help my family and provide for them.
+</p
+
+
+
+
+
